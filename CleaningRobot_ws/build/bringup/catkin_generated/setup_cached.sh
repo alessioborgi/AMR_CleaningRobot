61bbe707cb236d0dd94f27fcad6b1c2c761224ba
alessioborgi <borgi.1952442@studenti.uniroma1.it>
2024-02-08 22:45:33 +0100

    Versionn 0.2

--- CleaningRobot_ws/build/bringup/catkin_generated/setup_cached.sh
+++ CleaningRobot_ws/build/bringup/catkin_generated/setup_cached.sh
@@ -8,5 +8,7 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/ale/CleaningRobot_RP/CleaningRobot_ws/devel/.private/bringup:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/ale/CleaningRobot_RP/CleaningRobot_ws/devel/.private/bringup/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/ale/CleaningRobot_RP/CleaningRobot_ws/devel/.private/bringup/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/ale/CleaningRobot_RP/CleaningRobot_ws/build/bringup'
 export ROS_PACKAGE_PATH="/home/ale/CleaningRobot_RP/CleaningRobot_ws/src/bringup:$ROS_PACKAGE_PATH"
