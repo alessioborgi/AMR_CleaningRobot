16359b446ea5b4d4228febfb5c8bc3fe2692d545
alessioborgi <borgi.1952442@studenti.uniroma1.it>
2024-02-11 18:58:46 +0100

    Add Build of teleoperation

---
+++ CleaningRobot_ws/build/bringup/CMakeFiles/enable.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/ale/CleaningRobot_RP/CleaningRobot_ws/devel/.private/bringup/lib/bringup/enable"
+  "/home/ale/CleaningRobot_RP/CleaningRobot_ws/devel/.private/bringup/lib/bringup/enable.pdb"
+  "CMakeFiles/enable.dir/src/sensor_enable.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/enable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
