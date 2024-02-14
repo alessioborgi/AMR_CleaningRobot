22b50fcab3dc79b97e2e82961046ae9d614d1b81
alessioborgi <borgi.1952442@studenti.uniroma1.it>
2024-02-14 17:44:54 +0100

    Restoring v3 TeleOp

---
+++ CleaningRobot_ws/build/bringup/CMakeFiles/dynamic_link_camera.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/ale/CleaningRobot_RP/CleaningRobot_ws/devel/lib/bringup/dynamic_link_camera"
+  "/home/ale/CleaningRobot_RP/CleaningRobot_ws/devel/lib/bringup/dynamic_link_camera.pdb"
+  "CMakeFiles/dynamic_link_camera.dir/src/static_camera.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dynamic_link_camera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
