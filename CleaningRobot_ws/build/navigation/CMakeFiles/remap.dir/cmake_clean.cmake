af226864841dbb4aa5f04839c7056553daee8b2a
alessioborgi <borgi.1952442@studenti.uniroma1.it>
2024-02-20 13:37:07 +0100

    First commit of Obstacle Avoidance

---
+++ CleaningRobot_ws/build/navigation/CMakeFiles/remap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/ale/CleaningRobot_RP/CleaningRobot_ws/devel/lib/navigation/remap"
+  "/home/ale/CleaningRobot_RP/CleaningRobot_ws/devel/lib/navigation/remap.pdb"
+  "CMakeFiles/remap.dir/src/remapping.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/remap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
