e4b8e1f08f4f012a5aeeae95d2c2a1f0e8bdea8e
alessioborgi <borgi.1952442@studenti.uniroma1.it>
2024-02-24 18:28:49 +0100

    Modifued Local and Global Costmap

--- CleaningRobot_ws/src/nav_plan_objavoid/include/remapping.h
+++ CleaningRobot_ws/src/nav_plan_objavoid/include/remapping.h
@@ -1,7 +1,20 @@
 #ifndef REMAPPING_H
 #define REMAPPING_H
 
+/*
 
+File: remapping.h
+Author: @AlessioBorgi
+Date: 20-02-2024
+
+Description: This file contains the definition header for the Remapping class. 
+             The Remapping class is responsible for subscribing to sensor data related to laser scans 
+             and republishing them with remapped values.
+
+*/
+
+
+// Include necessary libraries
 #include "ros/ros.h"
 #include "webots_ros/Int32Stamped.h"
 #include <webots_ros/set_float.h>
@@ -13,21 +26,25 @@
 #include <iostream>
 #include <cmath>
 
+// Define constant for time step
 #define TIME_STEP 32
 
-
+// Define the Remapping class
 class Remapping{
+
     private:
-        ros::NodeHandle nh_;
-        std::string robot_name_;
-        ros::Subscriber subscribe_name_ , subscribe_laser_;
-        ros::Publisher publish_laser_;
-        sensor_msgs::LaserScan local_scan_msg_;
+        ros::NodeHandle nh_;                                // NodeHandle for ROS communication.
+        std::string robot_name_;                            // Variable to store robot name.
+        ros::Subscriber subscribe_name_ , subscribe_laser_; // ROS subscribers for topics.
+        ros::Publisher publish_laser_;                      // ROS publisher for laser scan topic.
+        sensor_msgs::LaserScan local_scan_msg_;             // LaserScan message for storing remapped values.
 
     public:
+        // Constructor
         Remapping(ros::NodeHandle* nodehandle);
-        void NameCallBack(const std_msgs::String& msg);
-        void LaserCallBack(const sensor_msgs::LaserScan& msg);
+        // Callback functions
+        void NameCallBack(const std_msgs::String& msg);         // Callback for /model_name topic.
+        void LaserCallBack(const sensor_msgs::LaserScan& msg);  // Callback for laser scan topic.
 };
 
 #endif
--- CleaningRobot_ws/src/nav_plan_objavoid/src/remapping.cpp
+++ CleaningRobot_ws/src/nav_plan_objavoid/src/remapping.cpp
@@ -1,31 +1,40 @@
 #include <remapping.h>
 
+/*
+
+File: remapping.cpp
+Author: @AlessioBorgi
+Date: 20-02-2024
+
+Description: This file contains the implementation for the Remapping Class. 
+             This class subscribes to sensor data related to laser scans and republishes them with remapped values.
+
+*/
+
+// Constructor: initializes ROS subscribers and publisher.
 Remapping::Remapping(ros::NodeHandle* nodehandle):nh_(*nodehandle){
-    subscribe_name_ = nh_.subscribe("/model_name", 1, &Remapping::NameCallBack,this);
-    publish_laser_ = nh_.advertise<sensor_msgs::LaserScan>("/LaserScan", 1000);
+    subscribe_name_ = nh_.subscribe("/model_name", 1, &Remapping::NameCallBack,this);   // Subscribe to /model_name topic.
+    publish_laser_ = nh_.advertise<sensor_msgs::LaserScan>("/LaserScan", 1000);         // Advertise publishing on /LaserScan topic.
 }
 
-
-
+// Callback for /model_name topic: stores the robot name and subscribes to laser scan topic.
 void Remapping::NameCallBack(const std_msgs::String& msg){
-    Remapping::robot_name_ = msg.data;
-    subscribe_laser_ = nh_.subscribe(robot_name_+"/Lidar/laser_scan/layer0", 1, &Remapping::LaserCallBack,this);
+    Remapping::robot_name_ = msg.data;                                                  // Store the robot name.
+    subscribe_laser_ = nh_.subscribe(robot_name_+"/Lidar/laser_scan/layer0", 1, &Remapping::LaserCallBack,this); // Subscribe to laser scan topic.
 }
 
-
-
+// Callback for laser scan topic: republishes the laser scan message with remapped values.
 void Remapping::LaserCallBack(const sensor_msgs::LaserScan& msg){
-    local_scan_msg_ = msg;
-    // local_scan_msg_.header.frame_id = "base_link";
-    publish_laser_.publish(local_scan_msg_);
+    local_scan_msg_ = msg;                            // Store the received laser scan message.
+    // local_scan_msg_.header.frame_id = "base_link"; // Optionally change the frame id.
+    publish_laser_.publish(local_scan_msg_);          // Publish the laser scan message.
 }
 
-
+// Main function: initializes ROS node, creates Remapping object, and spins ROS.
 int main(int argc, char **argv){
-    ros::init(argc, argv, "Remapping");
-    ros::NodeHandle nh;
-    Remapping member(&nh);
-    ros::spin();
-
+    ros::init(argc, argv, "Remapping"); // Initialize ROS node.
+    ros::NodeHandle nh;                 // Create ROS node handle.
+    Remapping member(&nh);              // Create Remapping object.
+    ros::spin();                        // Spin ROS.
     return 0;
 }
