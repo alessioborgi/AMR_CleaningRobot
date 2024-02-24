77ce390adff10856861be506164611b3b2b8dfc0
alessioborgi <borgi.1952442@studenti.uniroma1.it>
2024-02-24 16:35:36 +0100

    Renamed navigation to nav_plan_objavoid

---
+++ CleaningRobot_ws/build/nav_plan_objavoid/CMakeFiles/remap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/ale/CleaningRobot_RP/CleaningRobot_ws/devel/lib/nav_plan_objavoid/remap"
+  "/home/ale/CleaningRobot_RP/CleaningRobot_ws/devel/lib/nav_plan_objavoid/remap.pdb"
+  "CMakeFiles/remap.dir/src/remapping.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/remap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
